1b28805f8fc054b4718581a1f9e3df4b8628c863
Lystone19 <83387611+Lystone19@users.noreply.github.com>
2021-04-30 13:32:22 +0800

    Add files via upload

---
+++ BEEM061/First website/myfirstjsfunction.js
@@ -0,0 +1,13 @@
+function changeImage(){
+var image = document.getElementById('theImage');
+
+if
+(image.src.match("eatingdragon"))
+
+{image.src="drunk frog.jpg";}
+
+else
+{image.src="eatingdragon.jpg";}
+
+
+}
